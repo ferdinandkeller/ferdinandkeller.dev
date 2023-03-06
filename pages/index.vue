47c80b3df00d81484de7f81564db81bec36b6ac5
Ferdinand Keller <ferdinand.keller@proton.me>
2023-03-06 22:28:59 +0100

    added shadow circle on main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,93 @@
 <template>
   <div class="home">
     <div class="circles">
-      <div class="nice-circle-2"></div>
       <div class="nice-circle-1"></div>
+      <div class="nice-circle-2"></div>
+      <div class="nice-circle-3"></div>
+
+      <div class="nice-circle-4"></div>
+      <div class="nice-circle-5"></div>
+      <div class="nice-circle-6"></div>
     </div>
-    <p>I'm a datascientist and AI engineer at CareGame.</p>
-    <p>I like to solve problems, and when I can't find any, I invent my own. My way of doing things has always been to
-      observe, analyze, imagine, create & destroy, non-stop. And now this passion is also my job. I find it interesting to
-      optimize and simplify complex systems. I'm also a huge Rust fan 🦀.</p>
+    <h1>Hi, my name is Ferdinand</h1>
+    <p>I'm a datascientist and AI engineer at <NuxtLink to="https://caregame.com" target="blank">CareGame</NuxtLink>.</p>
+    <p>I like to solve problems and break things. I'm also a huge Rust fan 🦀.</p>
     <p>Don't hesitate to contact me, even if it's just to say hi 👋 (especially if you also like rust).</p>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.nice-circle-1,
-.nice-circle-2 {
+.circles {
   position: absolute;
-  top: -100px;
-  right: 100px;
-  width: 400px;
-  height: 400px;
-  border-radius: 50%;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
   z-index: -1;
+}
 
-  --c: rgb(246, 231, 185);
-  box-shadow: 0 0 200px var(--c);
-  background-color: var(--c);
-  filter: blur(60px);
-  opacity: .5;
+.nice-circle-1,
+.nice-circle-2,
+.nice-circle-3,
+.nice-circle-4,
+.nice-circle-5,
+.nice-circle-6 {
+  position: absolute;
+  width: 800px;
+  height: 800px;
+  border-radius: 50%;
+
+  background: radial-gradient(rgba(255, 49, 121, 0.2) 0%, rgba(255, 23, 89, 0.1) 30%, transparent 60%);
+  filter: blur(30px);
+}
+
+.nice-circle-1 {
+  top: -200px;
+  right: -300px;
 }
 
 .nice-circle-2 {
-  top: 400px;
-  right: 300px;
+  top: -100px;
+  right: -100px;
+  background: radial-gradient(rgba(79, 173, 255, 0.2) 0%, rgba(23, 112, 255, 0.1) 30%, transparent 60%);
+}
 
-  --c: rgb(138, 235, 246);
-  box-shadow: 0 0 200px var(--c);
-  background-color: var(--c);
-  filter: blur(60px);
-  opacity: .5;
+.nice-circle-3 {
+  top: 100px;
+  right: -200px;
+  width: 600px;
+  height: 600px;
+  background: radial-gradient(rgba(79, 173, 255, 0.2) 0%, rgba(23, 112, 255, 0.1) 30%, transparent 60%);
+}
+
+
+.nice-circle-4 {
+  bottom: -200px;
+  left: -50px;
+  width: 500px;
+  height: 500px;
+}
+
+.nice-circle-5 {
+  bottom: -300px;
+  left: 0px;
+  width: 800px;
+  height: 800px;
+  background: radial-gradient(rgba(79, 173, 255, 0.2) 0%, rgba(23, 112, 255, 0.1) 30%, transparent 60%);
+}
+
+.nice-circle-6 {
+  bottom: -100px;
+  left: -100px;
+  width: 600px;
+  height: 600px;
+  background: radial-gradient(rgba(79, 173, 255, 0.2) 0%, rgba(23, 112, 255, 0.1) 30%, transparent 60%);
+}
+
+.home {
+  height: 100%;
+  margin-top: 20rem;
+  width: 60%;
 }
 </style>
