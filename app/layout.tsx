73a1f9fe96bedcec9c01a4739311f303c2cf9eee
Ferdinand Keller <ferdinand.keller@proton.me>
2026-04-18 15:04:40 +0200

    feat: add new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin", "latin-ext"] })
 
 export const metadata: Metadata = {
   title: "personal portfolio",
+  icons: { icon: "/favicon.svg" }
 }
 
 export default function RootLayout({
